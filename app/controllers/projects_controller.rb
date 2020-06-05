7f78262ad3eb16c4dfa81e0a9c6d67297e40b8ef
mohsin <mohsin.mahmood@fliclicks.com>
2020-06-05 16:49:14 +0500

    Using Model Files to Create Database Column Default Values

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,8 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    # @projects = Project.all
+    @projects = Project.almost_complete
   end
 
   # GET /projects/1
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,13 @@
 class Project < ApplicationRecord
+    after_initialize :set_defaults
+
+    scope :almost_complete, -> { where("percent_complete> 75.0")}
+    scope :still_need_some_work, -> { where("percent_complete < 75.0")}
+    # Ex:- scope :active, -> {where(:active => true)}
+    # Ex:- scope :active, -> {where(:active => true)}
+
+
+    def set_defaults
+        self.percent_complete ||=0.0
+    end
 end
