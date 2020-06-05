496c951bede4ea0f35312f973a9fe0f2e7cce9d1
mohsin <mohsin.mahmood@fliclicks.com>
2020-06-05 16:54:59 +0500

    How to Integrate Validations in Rails with Model Files

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ApplicationRecord
     after_initialize :set_defaults
+    validates_presence_of :title, :description
 
     scope :almost_complete, -> { where("percent_complete> 75.0")}
     scope :still_need_some_work, -> { where("percent_complete < 75.0")}
